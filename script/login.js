179241e7d15a7b3c4e43bbe8e2cc5ea944a87e8f
abdullah01846 <mdabdullahh106@gmail.com>
2025-10-14 21:17:47 +0600

    project login from completed

---
+++ script/login.js
@@ -0,0 +1,19 @@
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const acountNumber = document.getElementById("Acount-Number").value;
+    const pin = document.getElementById("pin").value;
+    const convertedpin = parseInt(pin);
+
+    if (acountNumber.length === 11) {
+      if (convertedpin === 1234) {
+        window.location.href = "./main.html";
+      } else {
+          alert("pin Number not oky");
+      }
+    } 
+    else {
+        alert('need valid acount number')
+    }
+  });
