37c771a2f42a7e21c33d390b3743ef99d7e9aede
abdullah01846 <mdabdullahh106@gmail.com>
2025-10-15 13:11:59 +0600

    balance add completed

--- script/login.js
+++ script/login.js
@@ -2,9 +2,11 @@ document
   .getElementById("login-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
+    
     const acountNumber = document.getElementById("Acount-Number").value;
     const pin = document.getElementById("pin").value;
     const convertedpin = parseInt(pin);
+
 
     if (acountNumber.length === 11) {
       if (convertedpin === 1234) {
