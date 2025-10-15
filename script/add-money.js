fe2a5703f49af978b439096c776da0592767e630
abdullah01846 <mdabdullahh106@gmail.com>
2025-10-15 16:52:45 +0600

    cashout section done

--- script/add-money.js
+++ script/add-money.js
@@ -1,20 +1,36 @@
-document.getElementById("add-money")
-   .addEventListener("click", function (event) {
-       event.preventDefault();
-       const amount = document.getElementById('Acount').value;
-       const convertedAmount = parseInt(amount);
-       const pin = document.getElementById('pin').value;
-       const convertedPin = parseInt(pin);
-       const mainBalance = document.getElementById('main-balance').innerText
-       const convertedMainBalance = parseFloat(mainBalance);
+document
+  .getElementById("add-money")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const amount = document.getElementById("Acount").value;
+    const convertedAmount = parseInt(amount);
+    const pin = document.getElementById("pin").value;
+    const convertedPin = parseInt(pin);
+    const mainBalance = document.getElementById("main-balance").innerText;
+    const convertedMainBalance = parseFloat(mainBalance);
 
 
-       if(convertedPin===1234) {
-           const sum = convertedMainBalance + convertedAmount;
+    if(amount && pin){
+        if(convertedPin === 1234){
+            const sum = convertedMainBalance + convertedAmount;
             document.getElementById('main-balance').innerText = sum;
-       }
-       else{
-        console.log('pin shothik na')
-       }
+        }
+        else{
+            alert('enter valid pin')
+        }
+            
+    }
+    else{
+        alert('enter amount')
+    }
 
-   });
+
+
+
+    // if (convertedPin === 1234) {
+    //   const sum = convertedMainBalance + convertedAmount;
+    //   document.getElementById("main-balance").innerText = sum;
+    // } else {
+    //   console.log("pin shothik na");
+    // }
+  });
